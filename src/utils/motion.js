e8c1adee17ee2b699e7ba0b2ba59ae91bc9a2882
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-06-14 03:05:46 +0600

    added

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -57,22 +57,22 @@ export const zoomIn = (delay, duration) => {
 };
   
 export const slideIn = (direction, type, delay, duration) => {
-    return {
-      hidden: {
-        x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-        y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+  return {
+    hidden: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: "easeOut",
       },
-      show: {
-        x: 0,
-        y: 0,
-        transition: {
-          type: type,
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
-      },
-    };
+    },
+  };
 };
   
 export const staggerContainer = (staggerChildren, delayChildren) => {
